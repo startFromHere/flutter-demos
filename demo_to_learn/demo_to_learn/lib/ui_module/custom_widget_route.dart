fb8bf5b0394685a5add0ca29865e158f5312a9f0
liutao <liutao900629@gmail.com>
2023-02-16 21:44:05 +0800

    自定义组件1

--- demo_to_learn/demo_to_learn/lib/ui_module/custom_widget_route.dart
+++ demo_to_learn/demo_to_learn/lib/ui_module/custom_widget_route.dart
@@ -1,7 +1,16 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class CustomWidgetRoute extends StatelessWidget {
+class CustomWidgetRoute extends StatefulWidget {
   const CustomWidgetRoute({super.key});
+
+  @override
+  State<CustomWidgetRoute> createState() => _CustomWidgetRouteState();
+}
+
+class _CustomWidgetRouteState extends State<CustomWidgetRoute> {
+  double _turns = .0;
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +26,22 @@ class CustomWidgetRoute extends StatelessWidget {
                   colors: [Colors.orange, Colors.red],
                   child: Text('Submit'),
                   onPressed: onTap,
-                )
+                ),
+                TurnBox(
+                    turns: _turns,
+                    speed: 500,
+                    child: Icon(
+                      Icons.refresh,
+                      size: 50,
+                    )),
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _turns += 0;
+                      });
+                    },
+                    child: Text("顺时针旋转1/5圈")),
+                RepaintBoundary(child: CustomPaintWidget()),
               ],
             ),
           ),
@@ -84,4 +108,121 @@ class GradientButton extends StatelessWidget {
       ),
     );
   }
+}
+
+class TurnBox extends StatefulWidget {
+  const TurnBox(
+      {Key? key, this.turns = .0, this.speed = 200, required this.child})
+      : super(key: key);
+
+  final double turns;
+  final int speed;
+  final Widget child;
+
+  @override
+  State<TurnBox> createState() => _TurnBoxState();
+}
+
+class _TurnBoxState extends State<TurnBox> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, lowerBound: -double.infinity, upperBound: double.infinity);
+    _controller.value = widget.turns;
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RotationTransition(turns: _controller, child: widget.child);
+  }
+
+  @override
+  void didUpdateWidget(covariant TurnBox oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.turns != widget.turns) {
+      _controller.animateTo(widget.turns,
+          duration: Duration(milliseconds: widget.speed ?? 200),
+          curve: Curves.easeOut);
+    }
+  }
+}
+
+class CustomPaintWidget extends StatelessWidget {
+  const CustomPaintWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      size: Size(200, 200),
+      painter: MyPainter(),
+    );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    print("paint");
+    var rect = Offset.zero & size;
+
+    drawChessboard(canvas, rect);
+    drawPieces(canvas, rect);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
+
+void drawChessboard(Canvas canvas, Rect rect) {
+  var paint = Paint()
+    ..isAntiAlias = true
+    ..style = PaintingStyle.fill
+    ..color = Color(0xFFDCC48C);
+  canvas.drawRect(rect, paint);
+
+  paint
+    ..style = PaintingStyle.stroke
+    ..color = Colors.black38
+    ..strokeWidth = 1.0;
+
+  for (int i = 0; i <= 15; i++) {
+    double dy = rect.top + rect.height / 15 * i;
+    canvas.drawLine(Offset(rect.left, dy), Offset(rect.right, dy), paint);
+  }
+
+  for (int i = 0; i <= 15; i++) {
+    double dx = rect.left + rect.width / 15 * i;
+    canvas.drawLine(Offset(dx, rect.top), Offset(dx, rect.bottom), paint);
+  }
+}
+
+void drawPieces(Canvas canvas, Rect rect) {
+  double eWidth = rect.width / 15;
+  double eHeight = rect.height / 15;
+
+  var paint = Paint()
+    ..style = PaintingStyle.fill
+    ..color = Colors.black;
+
+  canvas.drawCircle(
+      Offset(rect.center.dx - eWidth / 2, rect.center.dy - eHeight / 2),
+      min(eWidth / 2, eHeight / 2) - 2,
+      paint);
+
+  paint.color = Colors.white;
+  canvas.drawCircle(
+      Offset(rect.center.dx + eWidth / 2, rect.center.dy - eHeight / 2),
+      min(eWidth / 2, eHeight / 2) - 2,
+      paint);
 }
